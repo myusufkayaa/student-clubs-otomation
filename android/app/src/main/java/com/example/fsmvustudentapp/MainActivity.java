fe51540bb01eea5eba6a0e25a00929dcebcee130
Barış Kışlak <baris.kislak@stu.fsm.edu.tr>
2018-12-06 22:02:10 +0300

    We added tabhost for android

---
+++ android/app/src/main/java/com/example/fsmvustudentapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.fsmvustudentapp;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new mainScreen()).commit();
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new mainScreen();
+                    break;
+                case R.id.navigation_food_list:
+                    selectedFragment = new foodList();
+                    break;
+                case R.id.navigation_clups:
+                    selectedFragment = new stuClubs();
+                    break;
+                case R.id.navigation_events:
+                    selectedFragment = new cluEvents();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container , selectedFragment).commit();
+
+            return true;
+        }
+    };
+}
